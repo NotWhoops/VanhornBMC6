afcc6da830d77d5e18df0b47d7694c985596d0e0
NotWhoops <whoopsplayz@gmail.com>
2026-01-15 15:45:56 -0800

    added magic 8 ball code

---
+++ Services/Magic8BallServices.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VanhornBMC6.Services
+{
+    public class Magic8BallServices
+    {
+        bool playAgain = true;
+        string answer = "";
+
+        string[] responses ={
+        "Probably not, don't ask again",
+        "How dare you, thats weird",
+        "Um no, you might want to seek therapy",
+        "Yes, I always knew you were a nerd",
+        "Nah",
+        "Yes but also no",
+        "System Error, Please try again",
+        "You monster, how dare you suggest that",
+        "I don't contain the parameters to help you",
+        "Ayo, please read the bible bro"
+        };
+
+        Random rand = new Random();
+
+        // while (playAgain)
+        // {
+        // return "Welcome to Magic 8 Ball! Please ask a question:";
+        // string? question = return;
+
+        // if (string.IsNullOrWhiteSpace(question))
+        // {
+        // return "You need to ask something!";
+        // continue;
+        // }
+
+        // if (!question.Trim().EndsWith("?"))
+        // {
+        // return "That doesn't look like a question. Add a ? to your sentence";
+        // continue;
+        // }
+    }
+}
--- obj/Debug/net9.0/Todov4.AssemblyInfo.cs
+++ obj/Debug/net9.0/Todov4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Todov4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de350debdcc1341a49b47e3369941ea5c82a406b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Todov4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Todov4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
