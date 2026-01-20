4ae547b0458109e67df24a99e0a5c38ed3c09636
NotWhoops <whoopsplayz@gmail.com>
2026-01-20 15:18:02 -0800

    services for guessit works

--- Controllers/GuessItController.cs
+++ Controllers/GuessItController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using VanhornBMC6.Services;
 
 namespace VanhornBMC6.Controllers
 {
@@ -10,6 +11,32 @@ namespace VanhornBMC6.Controllers
     [Route("api/[controller]")]
     public class GuessItController : ControllerBase
     {
-        
+        private readonly GuessItServices _guessItServices;
+
+            public GuessItController(GuessItServices guessItServices)
+            {
+                _guessItServices = guessItServices;
+            }
+            [HttpGet]
+            [Route("GuessItServicesEasy/{easyGuess}")]
+            public string GuessItServices(string easyGuess)
+            {
+                return _guessItServices.
+                GuessIt("easy", easyGuess);
+            }
+            [HttpGet]
+            [Route("GuessItServicesMedium/{mediumGuess}")]
+            public string GuessItServices(string mediumGuess)
+            {
+                return _guessItServices.
+                GuessIt("medium", mediumGuess);
+            }
+            // [HttpGet]
+            [Route("GuessItServicesHard/{hardGuess}")]
+            public string GuessItServices(string hardGuess)
+            {
+                return _guessItServices.
+                GuessIt("hard", hardGuess);
+            }
     }
 }
--- Services/GuessItServices.cs
+++ Services/GuessItServices.cs
@@ -7,205 +7,95 @@ namespace VanhornBMC6.Services
 {
     public class GuessItServices
     {
-        bool playAgain = true;
+        public string GuessIt(string diff, string easyGuess, string mediumGuess, string hardGuess)
+        {
+            Random random = new Random();
+            int NumberOfGuesses = 0;
 
-        // do
-        // {
-        //     bool playAgain1 = true;
-        //     Console.Clear();
-        //     int min = 1;
-        //     int max = 10;
-        //     Console.WriteLine("Welcome to the Guessing Game. Please Choose Easy, Medium, Hard or Custom");
-        //     string? userInput = Console.ReadLine();
-        //     Random random = new Random();
-        //     int NumberOfGuesses = 0;
+            switch (diff)
+            {
+                case "Easy":
+                   return Easy();
+                case "Medium":
+                   return Medium();
+                case "Hard":
+                   return Hard();
+                default:
+                  return "Invalid difficulty";
+            }
 
-        //     while (playAgain)
-        //     {
-        //         switch (userInput)
-        //         {
-        //             case "Easy":
-        //                 Easy();
-        //                 min = 1; max = 10;
-        //                 break;
-        //             case "Medium":
-        //                 Medium();
-        //                 min = 1; max = 50;
-        //                 break;
-        //             case "Hard":
-        //                 Hard();
-        //                 min = 1; max = 100;
-        //                 break;
-        //             case "Custom":
-        //                 Custom();
-        //                 break;
-        //             default:
-        //                 Console.WriteLine("Invalid input");
-        //                 CustomFunction();
-        //                 break;
-        //         }
-        //     }
+            string Easy()
+            {
+                int randomNumber = random.Next(1, 11);
+                bool isNum2 = int.TryParse(easyGuess, out int num1);
 
+                if (isNum2)
+                {
+                    NumberOfGuesses++;
+                    if (num1 < randomNumber)
+                    {
+                        return ($"{num1} is to low");
+                    }
+                    else if (num1 > randomNumber)
+                    {
+                        return ($"{num1} is to high");
+                    }
 
-        //     void Easy()
-        //     {
-        //         int randomNumber = random.Next(1, 11);
-        //         while (playAgain == true)
-        //         {
-        //             Console.WriteLine("Please guess a number between 1 and 10");
-        //             string? userSelection = Console.ReadLine();
-        //             bool isNum2 = int.TryParse(userSelection, out int num1);
+                    else if (num1 == randomNumber)
+                    {
+                        return ($"{num1} is correct in {NumberOfGuesses} guesses");
+                    }
+                }
+                    return "invalid input";
+            }
+            string Medium()
+            {
+                int randomNumber2 = random.Next(1, 51);
+                bool isNum2 = int.TryParse(mediumGuess, out int num1);
 
-        //             if (isNum2)
-        //             {
-        //                 NumberOfGuesses++;
-        //                 if (num1 < randomNumber)
-        //                 {
-        //                     Console.WriteLine($"{num1} is to low");
-        //                 }
-        //                 else if (num1 > randomNumber)
-        //                 {
-        //                     Console.WriteLine($"{num1} is to high");
-        //                 }
+                if (isNum2)
+                {
+                    NumberOfGuesses++;
+                    if (num1 < randomNumber2)
+                    {
+                        return ($"{num1} is to low");
+                    }
+                    else if (num1 > randomNumber2)
+                    {
+                        return ($"{num1} is to high");
+                    }
 
-        //                 else if (num1 == randomNumber)
-        //                 {
-        //                     Console.WriteLine($"{num1} is correct in {NumberOfGuesses} guesses");
-        //                     playAgain = false;
-        //                 }
-        //             }
-        //             else
-        //             {
-        //                 Console.WriteLine("Invalid input, please enter a number");
-        //             }
-        //         }
-        //     }
+                    else if (num1 == randomNumber2)
+                    {
+                        return ($"{num1} is correct in {NumberOfGuesses} guesses");
+                    }
+                }
+                return "invalid input";
+            }
+            string Hard()
+            {
+                int randomNumber3 = random.Next(1, 101);
+                bool isNum2 = int.TryParse(hardGuess, out int num1);
 
-        //     void Medium()
-        //     {
-        //         int randomNumber2 = random.Next(1, 51);
-        //         while (playAgain == true)
-        //         {
+                if (isNum2)
+                {
+                    NumberOfGuesses++;
+                    if (num1 < randomNumber3)
+                    {
+                        return ($"{num1} is to low");
+                    }
+                    else if (num1 > randomNumber3)
+                    {
+                        return ($"{num1} is to high");
+                    }
 
-        //             Console.WriteLine("Please guess a number between 1 and 50");
-        //             string? userSelection = Console.ReadLine();
-        //             bool isNum2 = int.TryParse(userSelection, out int num1);
-
-        //             if (isNum2)
-        //             {
-        //                 NumberOfGuesses++;
-        //                 if (num1 < randomNumber2)
-        //                 {
-        //                     Console.WriteLine($"{num1} is to low");
-        //                 }
-        //                 else if (num1 > randomNumber2)
-        //                 {
-        //                     Console.WriteLine($"{num1} is to high");
-        //                 }
-
-        //                 else if (num1 == randomNumber2)
-        //                 {
-        //                     Console.WriteLine($"{num1} is correct in {NumberOfGuesses} guesses");
-        //                     playAgain = false;
-        //                 }
-        //             }
-        //         }
-        //     }
-        //     void Hard()
-        //     {
-        //         int randomNumber3 = random.Next(1, 101);
-        //         while (playAgain == true)
-        //         {
-
-        //             Console.WriteLine("Please guess a number between 1 and 100");
-        //             string? userSelection = Console.ReadLine();
-        //             bool isNum2 = int.TryParse(userSelection, out int num1);
-
-        //             if (isNum2)
-        //             {
-        //                 NumberOfGuesses++;
-        //                 if (num1 < randomNumber3)
-        //                 {
-        //                     Console.WriteLine($"{num1} is to low");
-        //                 }
-        //                 else if (num1 > randomNumber3)
-        //                 {
-        //                     Console.WriteLine($"{num1} is to high");
-        //                 }
-
-        //                 else if (num1 == randomNumber3)
-        //                 {
-        //                     Console.WriteLine($"{num1} is correct in {NumberOfGuesses} guesses");
-        //                     playAgain = false;
-        //                 }
-        //             }
-        //         }
-        //     }
-        //     void Custom()
-        //     {
-        //         Console.WriteLine("Enter minimum number:");
-        //         int.TryParse(Console.ReadLine(), out min);
-
-        //         Console.WriteLine("Enter maximum number:");
-        //         int.TryParse(Console.ReadLine(), out max);
-        //         int randomNumber4 = random.Next(min, max + 1);
-
-        //         while (playAgain == true)
-        //         {
-        //             Console.WriteLine("Please guess a number ");
-        //             string? userSelection = Console.ReadLine();
-        //             bool isNum2 = int.TryParse(userSelection, out int num1);
-
-        //             if (isNum2)
-        //             {
-        //                 NumberOfGuesses++;
-        //                 if (num1 < randomNumber4)
-        //                 {
-        //                     Console.WriteLine($"{num1} is to low");
-        //                 }
-        //                 else if (num1 > randomNumber4)
-        //                 {
-        //                     Console.WriteLine($"{num1} is to high");
-        //                 }
-
-        //                 else if (num1 == randomNumber4)
-        //                 {
-        //                     Console.WriteLine($"{num1} is correct in {NumberOfGuesses} guesses");
-        //                     playAgain = false;
-        //                 }
-        //             }
-        //         }
-        //     }
-        //     void CustomFunction()
-        //     {
-        //         Console.WriteLine("Please enter: Easy, Medium, Hard or Custom");
-        //         userInput = Console.ReadLine();
-        //     }
-
-
-        //     while (playAgain1)
-        //     {
-        //         Console.WriteLine("Do you want to play again, Yes or No?");
-        //         string? userSelection = Console.ReadLine();
-
-        //         if (userSelection == "yes")
-        //         {
-        //             Console.WriteLine("Have fun");
-        //             playAgain1 = false;
-        //             playAgain = true;
-        //         }
-        //         else if (userSelection == "no")
-        //         {
-        //             Console.WriteLine("See you later");
-        //             playAgain1 = false;
-        //             playAgain = false;
-        //         }
-
-        //         if (userSelection != "yes" && userSelection != "no")
-        //         {
-        //             Console.WriteLine("Try Again. Please enter 'yes' or 'no' ");
-        //         }
-        //     }
-        // } while (playAgain);
+                    else if (num1 == randomNumber3)
+                    {
+                        return ($"{num1} is correct in {NumberOfGuesses} guesses");
+                    }
+                }
+                return "invalid input";
+            }
+        }
     }
 }
